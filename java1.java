d08f6f61544d8fe1ee2dbf37dd19de31f99587bd
Administrator <Administrator@speed.mshome.net>
2015-10-19 12:22:19 +0800

    updata

--- java1.java
+++ java1.java
@@ -4,7 +4,7 @@ public class java1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("HelloWord");
+		System.out.println("HelloWord11111");
 
 	}
 
